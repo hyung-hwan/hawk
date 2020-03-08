16ef342cb2ba909384ea66b6a8dc7dea47642600
hyunghwan.chung <hyunghwan.chung@f8746d37-bc09-4344-add2-be18d9271f4e>
2020-03-08 15:39:33 +0000

    moved scripts to the scripts directory. added a embedding sample in c
    git-svn-id: file:///var/lib/codepot/svnrepo/hawk/trunk@138 f8746d37-bc09-4344-add2-be18d9271f4e

--- hawk/samples/hawk02.c
+++ hawk/samples/hawk02.c
@@ -1,78 +1,3 @@
-# Hawk
-
-- [Language](#language)
-- [Basic Modules](#basic-modules)
-- [Embedding Guide](#embedding-guide)
-
-## Language <a name="language"></a>
-
-Hawk implements most of the AWK programming language elements with extensions.
-
-
-### Entry Point
-
-You may change the entry point of your script by setting a function name with @pragma entry.
-
-```
-@pragma entry main
-
-function main ()
-{
-	print "hello, world";
-}
-```
-
-### Value
-
-- unitialized value
-- integer
-- floating-point number
-- string
-- byte string
-- array
-
-### Module
-
-
-
-### Incompatibility with AWK
-
-#### Parameter passing
-
-In AWK, the caller can pass an uninitialized variable as a function parameter and get a changed value if the callled function sets it to an array.
-
-```
-function q(a) {a[1]=20; a[2]=30;}
-BEGIN { q(x); for (i in x) print i, x[i]; }
-```
-
-In Hawk, you can prefix the pramater name with & to indicate call-by-reference for the same effect.
-
-```
-function q(&a) {a[1]=20; a[2]=30;}
-BEGIN { q(x); for (i in x) print i, x[i]; }
-```
-
-Alternatively, you may form an array before passing it to a function.
-
-```
-function q(a) {a[1]=20; a[2]=30;}
-BEGIN { x[3]=99; q(x); for (i in x) print i, x[i]; }'
-```
-
-
-## Basic Modules <a name="basic-modules"></a>
-
-### sys
-
-### ffi
-
-### mysql
-
-## Embedding Guide <a name="embedding-guide"></a>
-
-
-```
 #include <hawk-std.h>
 #include <stdio.h>
 #include <string.h>
@@ -146,4 +71,4 @@ oops:
 	if (hawk) hawk_close (hawk);
 	return -1;
 }
-```
+ 
