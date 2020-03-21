66d461e664b9046d0eb324f06db3b238f14f9700
hyung-hwan <hyunghwan.chung@gmail.com>
2020-03-21 11:38:27 +0000

    fixed a fault in hawk-rbt.h

--- hawk/lib/hawk-rbt.h
+++ hawk/lib/hawk-rbt.h
@@ -338,7 +338,7 @@ HAWK_EXPORT void hawk_rbt_fini (
 #if defined(HAWK_HAVE_INLINE)
 static HAWK_INLINE void* hawk_rbt_getxtn (hawk_rbt_t* rbt) { return (void*)(rbt + 1); }
 #else
-#define hawk_rbt_getxtn(awk) ((void*)((hawk_rbt_t*)(rbt) + 1))
+#define hawk_rbt_getxtn(rbt) ((void*)((hawk_rbt_t*)(rbt) + 1))
 #endif
 
 /**
